aea74f4421404663a64d180cf0fde94cb7782450
yazanalqasem <yalqasem@ipass-mena.com>
2023-11-06 19:33:17 +0530

    Added model files

---
+++ ipass_ios/ipass_ios/ipass_ios.h
@@ -0,0 +1,18 @@
+//
+//  ipass_ios.h
+//  ipass_ios
+//
+//  Created by MOBILE on 06/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ipass_ios.
+FOUNDATION_EXPORT double ipass_iosVersionNumber;
+
+//! Project version string for ipass_ios.
+FOUNDATION_EXPORT const unsigned char ipass_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ipass_ios/PublicHeader.h>
+
+
